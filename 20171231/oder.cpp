1f7426a5587a7bcab91a6f6e1930f69490e8ce08
iwin2471 <iwin12372@gmail.com>
2017-12-31 16:59:14 +0900

    end oder

--- 20171231/oder.cpp
+++ 20171231/oder.cpp
@@ -1,32 +1,59 @@
 #include <stdio.h>
 #include <string.h>
+int n, r, f, s;
+int p = 0, c = 0;
+char p_c[2], c_c[2];
+char alpha[27];
+int chk[27];
+char ans[26];
 
-void swap(char *x, char *y){
-	char temp;
-	temp = *x;
-	*x = *y;
-	*y = temp;
-}
-
-
-void permute(char *a, int l, int r){
-	int i;
-	if (l == r)
-		printf("%s\n", a);
-
-	else{
-		for (i = l; i <= r; i++){
-			swap(a + l, a + i);
-			permute(a, l + 1, r);
-			swap((a + l), (a + i));
+void permute(int now) {
+	if(now == r){
+		if (++p == f) {
+			for (int i = 0; i < r; i++)
+				printf("%c", ans[i]);
+			printf("\n");
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (chk[i] == 0) {
+			ans[now] = i+65;
+			chk[i] = 1;
+			permute(now+1);
+			chk[i] = 0;
 		}
 	}
 }
 
-int main(){
-	char str[] = "ABCDEFG";
-	int n = strlen(str);
-	permute(str, 0, n - 1);
-	getchar();
+void combination(char *arr, int index, char *data, int i){
+	if (index == r){
+		if (++c == s) {
+			for (int j = 0; j < r; j++)
+			  printf("%c", data[j]);
+			return;
+		}
+		return;
+	}
+
+	if (i >= n)
+		return;
+
+	data[index] = arr[i];
+	combination(arr, index + 1, data, i + 1);
+	combination(arr, index, data, i + 1);
+}
+
+int main() {
+	int i;
+	char data[26];
+	scanf("%d %d", &n, &r);
+	scanf("%d %d", &f, &s);
+
+	for (i = 0; i < n; i++)
+		alpha[i] = 65 + i;
+
+	permute(0);
+	combination(alpha, 0, data, 0);
+
 	return 0;
 }
