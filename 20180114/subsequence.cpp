1b84fb650da6af8426eaa1a33f6098e4825997dd
iwin2471 <iwin12372@gmail.com>
2018-01-14 17:43:02 +0900

    subsequence 5th solve

--- 20180114/subsequence.cpp
+++ 20180114/subsequence.cpp
@@ -5,58 +5,38 @@
 using namespace std;
 int input[2];
 vector<int> arr;
-int middle;
-int cnt;
-int f, s;
-
-int check(int first, int last) {
-	int sum = 0;
-	int tmp_cnt = 0;
-
-	for (int i = first; i <= last; i++) {
-		sum += arr[i];
-		tmp_cnt++;
-		if (sum >= input[1])
-			return tmp_cnt;
-	}
-	return 0;
-}
-
-void subsequence(int first, int last) {
-	if (last - first == 2) return;
-	middle = ((first + 1) + (last + 1)) / 2 - 1;
-	f = check(first, middle); //0
-	s = check(middle, last); //3
-	if (f < s && f !=0) {
-		cnt = f;
-		subsequence(first, middle);
-		return;
-	}
-	else if(s < f && s !=0){
-		cnt = s;
-		subsequence(middle, last);
-		return;
-	}
-	else {
-		cnt = s > f ? s : f;
-		if(cnt == f)
-			subsequence(first, middle);
-		else
-			subsequence(middle, last);
-	}
-}
 
 int main() {
 	int tmp;
+	int sum = 0;
+	int cnt = INT_MAX;
+	int tmp_cnt;
+
 	scanf("%d %d", &input[0], &input[1]);
 	for (int i = 0; i < input[0]; i++) {
 		scanf("%d", &tmp);
 		arr.push_back(tmp);
 	}
+	
+	for (int i = 0; i < input[0]; i++) {
+		sum += arr[i];
+		tmp = sum;
+		tmp_cnt = 0;
+
+		for (int j = i; j < input[0]; j++) {
+			sum += arr[j];
+			tmp_cnt++;
+			if (sum >= input[1]) {
+				if (tmp_cnt < cnt)
+					cnt = tmp_cnt;
+				break;
+			}
+		}
+		sum = tmp - arr[i];
+	}
 
 	if (find(arr.begin(), arr.end(), input[1]) != arr.end())
 		printf("1");
 	else
-		subsequence(0,input[0] - 1);
-	printf("%d", cnt);
+		printf("%d", cnt+1);
 }
