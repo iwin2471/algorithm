34cd0cf0a8023171942c56c1a0862d25c86aec36
iwin2471 <iwin12372@gmail.com>
2018-01-21 17:42:03 +0900

    tw other answer

--- 20180114/tw.cpp
+++ 20180114/tw.cpp
@@ -1,14 +1,13 @@
 
 #include<stdio.h>
 #include<string.h>
-#include <vector>
 using namespace std;
 
 #define SIZE 500000
 long long int top = 0;
 long long int tower[SIZE] = { 0, };
+long long int input[SIZE];
 long long int answer[SIZE] = { 0, };
-vector<int> arr;
 
 void push(long long int caw_num) {
 	if (top >= SIZE)
@@ -26,21 +25,16 @@ void pop() {
 
 int main() {
 	long long int int_input, j = 0;
-	long long int tmp;
-	long long int now_arr;
 	scanf("%lld", &int_input);
-	do {
-		scanf("%lld", &tmp);
-		arr.push_back(tmp);
-		j++;
-	} while (j < int_input);
 
-	for (long long int i = 1; i < int_input; i++) {
-		while (tower[top - 1] <= arr[i] && top > 0)
+	while (scanf("%lld", &input[j++]) && j < int_input);
+
+	for (long long int i = int_input - 1; i > -1; i--) {
+		while (input[i] >= input[tower[top - 1] - 1] && top > 0) {
+			answer[tower[top - 1] - 1] = i + 1;
 			pop();
-		if (top > 0 && arr[i] < tower[top - 1])
-			answer[i] = (long long)(find(arr.begin(), arr.end(), tower[top - 1]) - arr.begin() + 1);
-		push(arr[i]);
+		}
+		push(i + 1);
 	}
 
 	for (long long int i = 0; i < int_input; i++)
